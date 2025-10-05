1b5ae4c680b11ca6264400ba1407e892e9fccd76
Imran Qasim <imran.qasim@Imrans-MacBook-Pro.local>
2025-10-05 12:52:58 +0500

    Error Handling & Accessibility Implementation

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,6 +1,8 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+
 const EditInvoicePage = async (props: { params: Promise<{ id: string }> }) => {
     const params = await props.params;
     const id = params.id;
@@ -8,6 +10,9 @@ const EditInvoicePage = async (props: { params: Promise<{ id: string }> }) => {
         fetchInvoiceById(id),
         fetchCustomers(),
     ]);
+    if (!invoice) {
+        notFound();
+    }
     return (
         <main>
             <Breadcrumbs
